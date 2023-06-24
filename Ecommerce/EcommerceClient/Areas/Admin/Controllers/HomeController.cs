1ed29f8c4f1e0514946250060f9e9557a51c2f6b
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-06-25 01:48:11 +0700

    VinhLQ_25/06/2023_1
    Upload assets

---
+++ Ecommerce/EcommerceClient/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceClient.Areas.Admin.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
