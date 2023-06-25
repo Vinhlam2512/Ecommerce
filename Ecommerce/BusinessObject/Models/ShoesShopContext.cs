fa1df52261f75af766cb531fe1e32fffe9b42139
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-06-26 01:18:50 +0700

    add ignore

--- Ecommerce/BusinessObject/Models/ShoesShopContext.cs
+++ Ecommerce/BusinessObject/Models/ShoesShopContext.cs
@@ -33,7 +33,8 @@ namespace BusinessObject.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("server =DESKTOP-NB2DNI9\\VINH; database=ShoesShop;uid=sa;pwd=123456789;");
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server =103.184.112.242,1433; database=ShoesShop;uid=vinhlq;pwd=123456789;");
             }
         }
 
---
+++ Ecommerce/EcommerceClient/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceClient.Areas.Admin.Controllers {
+    public class LoginController : Controller {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
